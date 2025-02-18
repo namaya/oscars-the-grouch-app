0c6ecc3fd9eeffd2f90d7bb783a5705caf63488f
namaya <n3amaya@gmail.com>
2025-02-17 21:08:15 -0600

    feat: add game list screen

---
+++ app/src/main/java/com/namaya/oscarsthegrouch_app/GamesListViewAdapter.kt
@@ -0,0 +1,57 @@
+package com.namaya.oscarsthegrouch_app
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.namaya.oscarsthegrouch_app.databinding.GameCardBinding
+
+class Game(val name: String)
+
+class GamesListViewAdapter: RecyclerView.Adapter<GamesListViewAdapter.ViewHolder>(){
+    private val gamesList = mutableListOf(
+        Game("Game 1"),
+        Game("Game 2"),
+    )
+
+    inner class ViewHolder(val viewBinding: GameCardBinding): RecyclerView.ViewHolder(viewBinding.root)
+
+    /**
+     * Create a new view holder.
+     *
+     * This function is called by the runtime as part of constructing the views for the list. There
+     * will only ever be a static number of view holders regardless of the size of the data list (when
+     * list size > screen size).
+     *
+     * @param parent ?
+     * @param viewType ?
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val viewBinding = GameCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(viewBinding)
+    }
+
+    /**
+     * Get the size of the campaign list.
+     *
+     * @return The number of elements in the campaign list.
+     */
+    override fun getItemCount(): Int {
+        return gamesList.size
+    }
+
+    /**
+     * Bind an element to a view holder.
+     *
+     * This function is called by the runtime when a new element needs to be displayed in the campaign list.
+     *
+     * @param holder The view holder to bind the new element to.
+     * @param position The position of the element in the data list.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        require(position >= 0 && position < gamesList.size) { "Invalid position in campaign list." }
+
+        val campaignCardViewBinding = holder.viewBinding
+
+        campaignCardViewBinding.gameCardName.text = gamesList[position].name
+    }
+}
--- app/src/main/java/com/namaya/oscarsthegrouch_app/MainActivity.kt
+++ app/src/main/java/com/namaya/oscarsthegrouch_app/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.namaya.oscarsthegrouch_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.gamesListScreen.gamesListView.layoutManager = layoutManager
+        binding.gamesListScreen.gamesListView.adapter = GamesListViewAdapter()
     }
 }
