f5e145277306f211f7204f3986e2dc11aa12e180
namaya <n3amaya@gmail.com>
2025-02-22 15:02:33 -0600

    feat(gameslist): add addgamemenu

--- app/src/main/java/com/namaya/oscarsthegrouch_app/MainActivity.kt
+++ app/src/main/java/com/namaya/oscarsthegrouch_app/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.namaya.oscarsthegrouch_app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,5 +28,33 @@ class MainActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         binding.gamesListScreen.gamesListView.layoutManager = layoutManager
         binding.gamesListScreen.gamesListView.adapter = GamesListViewAdapter()
+
+        binding.gamesListScreen.addGameButton.setOnClickListener { view ->
+            val addGameMenu = PopupMenu(this, view)
+
+            // Inflate the menu resource
+            addGameMenu.menuInflater.inflate(R.menu.add_game_menu, addGameMenu.menu)
+
+            // Set an item click listener
+            addGameMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.addGameCreate -> {
+                        // Handle "Create" action
+                        Toast.makeText(this, "Create", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.addGameJoin -> {
+                        // Handle "Join" action
+                        Toast.makeText(this, "Join", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            // Show the PopupMenu
+            addGameMenu.show()
+        }
+
     }
 }
